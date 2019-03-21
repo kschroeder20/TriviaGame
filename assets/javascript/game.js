e4063c29a5fc5c1b4d1fe5e4c96650c4f8181b58
Kevin Schroeder <schroederkevin94@gmail.com>
2019-03-21 08:59:11 -0400

    WIP added final result page and centered game on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,18 +47,18 @@ var game = {
     questionsAnsweredCount: 0
 }
 
-  //For all of the other questions, run this function
-  var newQuestion = function (number) {
-      var questionNumber = number
-      $("#options-div").css('display', 'block');
-      $("#result-div").css('display', 'none');
-      $("#start-game").css('display', 'none');
-      $("#question").text(questions[questionNumber].question).removeClass('blue-button');
-      $("#option1").text(questions[questionNumber].option1).removeClass('blue-button');
-      $("#option2").text(questions[questionNumber].option2).removeClass('blue-button');
-      $("#option3").text(questions[questionNumber].option3).removeClass('blue-button');
-      $("#option4").text(questions[questionNumber].option4).removeClass('blue-button');
-  }
+//For all of the other questions, run this function
+var newQuestion = function (number) {
+    var questionNumber = number
+    $("#options-div").css('display', 'block');
+    $("#result-div").css('display', 'none');
+    $("#start-game").css('display', 'none');
+    $("#question").text(questions[questionNumber].question).removeClass('blue-button');
+    $("#option1").text(questions[questionNumber].option1).removeClass('blue-button');
+    $("#option2").text(questions[questionNumber].option2).removeClass('blue-button');
+    $("#option3").text(questions[questionNumber].option3).removeClass('blue-button');
+    $("#option4").text(questions[questionNumber].option4).removeClass('blue-button');
+}
 
 //Start game button
 $("#start-game").on("click", function () {
@@ -67,57 +67,57 @@ $("#start-game").on("click", function () {
     game.timerRunning = true;
     //Generate question
     newQuestion(game.questionsAnsweredCount);
-    });
-    
+});
 
-  
+//Capture option that the user selects
+$("button").on('click', function () {
+    //Toggle between blue background for button
+    if ($(this).hasClass("choice") && game.gameRunning === true && game.choiceClicked === false) {
+        game.choiceClicked = true;
+        game.finalChoice = $(this).val();
+        $(this).addClass("blue-button");
+    } else if ($(this).hasClass("choice") && game.gameRunning === true && game.choiceClicked === true) {
+        game.choiceClicked = true;
+        game.finalChoice = $(this).val();
+        $(".choice").removeClass("blue-button");
+        $(this).addClass("blue-button");
 
+    }
+});
+//When user submits the answer
+$("#submit-button").on('click', function () {
+    //Hide options
+    $("#options-div").css('display', 'none');
+    //Show result
+    $("#result-div").css('display', 'block');
+    if (game.finalChoice === questions[game.questionsAnsweredCount].answer) {
+        $('#result-text').text("You are correct!")
+        game.correctAnswerCounter = game.correctAnswerCounter + 1;
+    } else {
+        $('#result-text').text("You are incorrect :(")
+    }
+})
 
-    //Capture option that the user selects
-    $("button").on('click', function () {
-        //Toggle between blue background for button
-        if ($(this).hasClass("choice") && game.gameRunning === true && game.choiceClicked === false) {
-            game.choiceClicked = true;
-            game.finalChoice = $(this).val();
-            $(this).addClass("blue-button");
-        } else if ($(this).hasClass("choice") && game.gameRunning === true && game.choiceClicked === true) {
-            game.choiceClicked = true;
-            game.finalChoice = $(this).val();
-            $(".choice").removeClass("blue-button");
-            $(this).addClass("blue-button");
-
-        }
-    });
-    //When user submits the answer
-    $("#submit-button").on('click', function () {
-        //Hide options
+//When Next button is clicked on the results page, add one to the questions answered count
+$('#next-button').on('click', function () {
+    game.questionsAnsweredCount = game.questionsAnsweredCount + 1
+    //Alert if game is over - Temp
+    if (game.questionsAnsweredCount === questions.length && game.gameRunning === true) {
+        game.gameRunning = false;
+        //Hide options and question results
         $("#options-div").css('display', 'none');
-        //Show result
-        $("#result-div").css('display', 'block');
-        if (game.finalChoice === questions[game.questionsAnsweredCount].answer) {
-            $('#result-text').text("You are correct!")
-            game.correctAnswerCounter = game.correctAnswerCounter + 1;
-        } else {
-            $('#result-text').text("You are incorrect :(")
-        }
-    })
+        $("#result-div").css('display', 'none');
+        //Show final result
+        $("#final-div").css('display', 'block');
+        $("#final-div").text("Game over! You got "+ game.correctAnswerCounter + " out of 5 questions correct.")
+    } else {
+        newQuestion(game.questionsAnsweredCount);
+    }
+});
+//If answer is wrong, replace button with fail gif and add 1 to wrong answer count
 
-    //When Next button is clicked on the results page, add one to the questions answered count
-    $('#next-button').on('click', function () {
-        game.questionsAnsweredCount = game.questionsAnsweredCount + 1
-        //Alert if game is over - Temp
-        if (game.questionsAnsweredCount === questions.length && game.gameRunning === true) {
-            game.gameRunning = false;
-            alert("Game is Over");
-        } else {
-            newQuestion(game.questionsAnsweredCount);
-        }
-    });
-    //If answer is wrong, replace button with fail gif and add 1 to wrong answer count
+//If answer is right, replace buttons with success gif and add 1 to right answer count
 
-    //If answer is right, replace buttons with success gif and add 1 to right answer count
+//Move to next question
 
-    //Move to next question
-
-    //Return score at the end
-
+//Return score at the end
