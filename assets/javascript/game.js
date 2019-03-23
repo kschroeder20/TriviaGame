8d1b6f35886a4231e691bd844b7a093fda45fb2b
Kevin Schroeder <schroederkevin94@gmail.com>
2019-03-23 10:13:03 -0400

    v1 complete with media queries :: solved issue with timer speeding up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,70 +6,99 @@ var questions = [{
     option3: "Chris Bosh",
     option4: "DeMar DeRozan",
     answer: "2",
-    correctAnswer: "Jalen Rose"
+    correctAnswer: "Jalen Rose",
+    correctGif: '<iframe src="https://giphy.com/embed/INZUj2jZSruso" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/7QMHhcWu93NsY" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "What language did Amar'e Stoudemire speak on the court so no one could understand him?",
     option1: "Hebrew",
     option2: "Greek",
     option3: "Gaelic",
     option4: "Latin",
-    answer: "1"
+    answer: "1",
+    correctAnswer: "Hebrew",
+    correctGif: '<iframe src="https://giphy.com/embed/l3q2IGmSdIMKaW1J6" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/1ppmWNzRPZo6Vi9Cg4" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "Who has the most NBA career three pointers?",
     option1: "Steph Curry",
     option2: "James Harden",
     option3: "Kobe Bryant",
     option4: "Patrick Ewing",
-    answer: "4"
+    answer: "4",
+    correctAnswer: "Patrick Ewing",
+    correctGif: '<iframe src="https://giphy.com/embed/d1ohGg1rWKOOc" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/mBEjTX3CP9M7C" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "Who has the most Shaqtin' a Fool MVP's?",
     option1: "JaVale McGee",
     option2: "LeBron James",
     option3: "Lonzo Ball",
     option4: "Nick Young",
-    answer: "1"
+    answer: "1",
+    correctAnswer: "Javale McGee",
+    correctGif: '<<iframe src="https://giphy.com/embed/1BGBv47q2xqEFNRH6p" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<<iframe src="https://giphy.com/embed/eG7KXuFaVrnVu" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "Within how many years of retirement are most NBA players broke?",
     option1: "8",
     option2: "2",
     option3: "10",
     option4: "5",
-    answer: "4"
+    answer: "4",
+    correctAnswer: "5 years",
+    correctGif: '<<iframe src="https://giphy.com/embed/2wV6SspOmNSuo8wG5L" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/5gWGDxXSFcCoSWFQ2g" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "Where did Ron Artest apply to during his rookie season with the Bulls in order to get an employee discount?",
     option1: "Burger King",
     option2: "Publix",
     option3: "Circut City",
     option4: "Apple",
-    answer: "3"
+    answer: "3",
+    correctAnswer: "Circut City",
+    correctGif: '<<iframe src="https://giphy.com/embed/8gsE6LF92vJiE" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/yEZMznoPiKhgc" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "Who averaged a record 5.6 blocks per game in one season?",
     option1: "Mark Eaton",
     option2: "Hakeem Olajuwon",
     option3: "Dikembe Mutombo",
     option4: "Alonzo Mourning",
-    answer: "1"
+    answer: "1",
+    correctAnswer: "Mark Eaton",
+    correctGif: '<iframe src="https://giphy.com/embed/y65VoOlimZaus" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/y65VoOlimZaus" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "What NBA great owns 10% of all 'Five Guys Burgers' restaurants in the US?",
     option1: "Steve Nash",
     option2: "Shaquille O'Neal",
     option3: "Michael Jordan",
     option4: "Magin Johnson",
-    answer: "2"
+    answer: "2",
+    correctAnswer: "Shaquille O'Neal",
+    correctGif: '<iframe src="https://giphy.com/embed/E3oIfcPa9FiJa" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/11RGwPkgzAs4F2" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>'
 }, {
     question: "Who has missed the most Career field goals in NBA history?",
     option1: "LeBron James",
     option2: "Ray Allen",
     option3: "Wilt Chamberland",
     option4: "Kobe Bryant",
-    answer: "4"
+    answer: "4",
+    correctAnswer: "Kobe Bryant",
+    correctGif: '<iframe src="https://giphy.com/embed/7rqiYoGahYG3K" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/xT1XGESDlxj0GwoDRe" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }, {
     question: "What NBA player said 'I'm the only player in the NBA who makes less money than his father'?",
     option1: "Austin Rivers",
     option2: "Steph Curry",
     option3: "Bill Laimbeer",
     option4: "Kevin Love",
-    answer: "4"
+    answer: "3",
+    correctAnswer: "Bill Laimbeer",
+    correctGif: '<iframe src="https://giphy.com/embed/l4FGHlaFW0VLSmUW4" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+    incorrectGif: '<iframe src="https://giphy.com/embed/mBEjTX3CP9M7C" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 }]
 
 //Create global variables
@@ -86,12 +115,6 @@ var game = {
     currentPage: 0 //currentPage is used to determine if the user is on a quiz page (0) or a result page (1)
 }
 
-var gifArrInt = Math.round(Math.random() * 3);
-
-var correctGifs = ['../images/correct1.gif', '../images/correct2.gif', '../images/correct3.gif', '../images/correct4.gif', '../images/correct5.gif'];
-
-var incorrectGifs = ['../images/fail1.gif', '../images/fail2.gif', '../images/fail3.gif', '../images/fail4.gif', '../images/fail5.gif'];
-
 
 //Start game button
 $("#start-game").on("click", function () {
@@ -104,11 +127,15 @@ $("#start-game").on("click", function () {
 var newQuestion = function (number) {
     //Start timer when the start game button is pressed
     $('#timer').text('00:30');
-    start();
+    startThirty();
     //Set current page to quiz page
     game.currentPage = 0;
+
+    //Show question
     var questionNumber = number;
+    //Show question options
     $("#options-div").css('display', 'block');
+    //Hide result, gifs, and start game content
     $("#result-div").css('display', 'none');
     $("#start-game").css('display', 'none');
     $("#result-gif").css('display', 'none');
@@ -119,8 +146,8 @@ var newQuestion = function (number) {
     $("#option4").text(questions[questionNumber].option4).removeClass('blue-button');
 }
 
-$(".choice").on('click', function (){
-//Capture option that the user selects
+$(".choice").on('click', function () {
+    //Capture option that the user selects
     game.choiceClicked = true;
     game.finalChoice = $(this).val();
     //Stop Timer
@@ -131,95 +158,71 @@ $(".choice").on('click', function (){
     $("#result-div").css('display', 'block');
     //Set current page to results page
     game.currentPage = 1;
-    //Add one to questionsAnswered count
     //if the final choice equals the value of the question's answer
     if (game.finalChoice === questions[game.questionsAnsweredCount].answer) {
+        //Show Results
         $("#result-gif").css('display', 'block');
-        $("#result-gif").html(correctGifs[gifArrInt]);
+        //Remove any existing Gif from last question
+        $("#result-gif").html("");
+        //Add Gif depending on question
+        $("#result-gif").html(questions[game.questionsAnsweredCount].correctGif);
         $('#result-text').text("You are correct!");
-        //start timer
-        start();
+        //Add one to correct answer count
+        game.correctAnswerCounter++
+        //Start timer
+        startTen();
     } else {
+        //Show Results
         $("#result-gif").css('display', 'block');
-        $("#result-gif").html(incorrectGifs[gifArrInt]);
-        $('#result-text').text("You are incorrect :(. The correct answer is " + questions[game.questionsAnsweredCount].correctAnswer);
-        start();
+        //Remove any existing Gif from last question
+        $("#result-gif").html("");
+        //Add Gif depending on question
+        $("#result-gif").html(questions[game.questionsAnsweredCount].incorrectGif);
+        //Show correct answer
+        $('#result-text').text("You are incorrect. The correct answer is " + questions[game.questionsAnsweredCount].correctAnswer);
+        //Start timer
+        startTen();
     }
+    //Add one to questionsAnswered count
     game.questionsAnsweredCount = game.questionsAnsweredCount + 1;
 });
 
 function noSelect() {
+    //Stop Timer
     stop();
     //Hide options
     $("#options-div").css('display', 'none');
     //Show result
     game.currentPage = 1;
-    game.questionsAnsweredCount = game.questionsAnsweredCount + 1;
-    start();
-    $("#result-div").css('display', 'block');
     $("#result-gif").css('display', 'block');
-    $("#result-gif").html(incorrectGifs[gifArrInt]);
-    $('#result-text').text("You are incorrect :(");
+    //Remove any existing Gif from last question
+    $("#result-gif").html("");
+    //Add Gif depending on question
+    $("#result-gif").html(questions[game.questionsAnsweredCount].incorrectGif);
+    //Show correct answer
+    $('#result-text').text("You are incorrect. The correct answer is " + questions[game.questionsAnsweredCount].correctAnswer);
+    //Start timer
+    startTen();
+    //Add one to questionsAnswered count
+    game.questionsAnsweredCount = game.questionsAnsweredCount + 1;
 
 }
 
-//When user submits the answer
-// $("#submit-button").on('click', function () {
-//     //Stop Timer
-//     game.timerRunning = false;
-//     //Hide options
-//     $("#options-div").css('display', 'none');
-//     //Show result
-//     $("#result-div").css('display', 'block');
-//     //Set current page to results page
-//     game.currentPage = 1;
-//     if (game.finalChoice === questions[game.questionsAnsweredCount].answer) {
-//         $("#result-gif").css('display', 'block');
-//         $("#result-gif").html(correctGifs[gifArrInt]);
-//         $('#result-text').text("You are correct!");
-//         game.correctAnswerCounter = game.correctAnswerCounter + 1;
-//     } else {
-//          $("#result-gif").css('display', 'block');
-//          $("#result-gif").html(incorrectGifs[gifArrInt]);
-//         $('#result-text').text("You are incorrect :(")
-//     }
-// })
-
-//When Next button is clicked on the results page, add one to the questions answered count
-// $('#next-button').on('click', function () {
-//     game.questionsAnsweredCount = game.questionsAnsweredCount + 1
-//     //Alert if game is over - Temp
-//     if (game.questionsAnsweredCount === questions.length && game.gameRunning === true) {
-//         game.gameRunning = false;
-//         //Hide options and question results
-//         $("#options-div").css('display', 'none');
-//         $("#result-div").css('display', 'none');
-//         $("#result-gif").css('display', 'none');
-
-//         //Show final result
-//         $("#final-div").css('display', 'block');
-//         $("#final-div").text("Game over! You got " + game.correctAnswerCounter + " out of 10 questions correct.")
-//     } else {
-//         newQuestion(game.questionsAnsweredCount);
-//     }
-// });
-//If answer is wrong, replace button with fail gif and add 1 to wrong answer count
-
-//If answer is right, replace buttons with success gif and add 1 to right answer count
-
-//Move to next question
-
-//Return score at the end
 //Timer Functions
-function start() {
+function startThirty() {
     game.time = 30;
-    if (game.timerRunning === false && game.currentPage !== 1) {
-        game.timerRunning = true;
-        game.intervalId = setInterval(count, 50);
-    } else if (game.timerRunning === false && game.currentPage === 1) {
-        game.timerRunning = true;
-        game.intervalId = setInterval(count, 50);
-    }
+    game.timerRunning = true;
+    //Clear anything from last question
+    clearInterval(game.intervalId);
+    game.intervalId = setInterval(count, 10);
+}
+
+function startTen() {
+    game.time = 5;
+    game.timerRunning = true;
+    //Clear anything from last question
+    clearInterval(game.intervalId);
+    game.intervalId = setInterval(count, 10);
 }
 
 function stop() {
@@ -234,19 +237,34 @@ function count() {
     game.time = game.time - 1;
     // Get the current time, pass that into the timeConverter function, and save the result in a variable.
     var newTime = timeConverter(game.time);
-    //  TODO: Use the variable you just created to show the converted time in the "display" div.
+    //Show time on page
     $("#timer").text(newTime);
-    if (game.time === 0 && game.currentPage === 1) {
+    //If time runs out, the user is on a results page, and there are more questions, generate a new question
+    if (game.time === 0 && game.currentPage === 1 && game.questionsAnsweredCount < questions.length) {
         stop();
         newQuestion(game.questionsAnsweredCount);
-    } else if (game.time === 0 && game.currentPage === 0) {
+    }
+    //If time runs out, the user is on a quiz page, and there are more questions, generate a new question
+    else if (game.time === 0 && game.currentPage === 0 && game.questionsAnsweredCount < questions.length) {
         stop();
         noSelect();
+
+    }
+    //If time runs out, the user is on a results page, and there are no more questions, show final results
+    else if (game.time === 0 && game.currentPage === 1 && game.questionsAnsweredCount === questions.length) {
+        game.gameRunning = false;
+        stop();
+        //Hide options and question results
+        $("#options-div").css('display', 'none');
+        $("#result-div").css('display', 'none');
+        $("#result-gif").css('display', 'none');
+
+        //Show final result
+        $("#final-div").css('display', 'block');
+        $("#replay-button").css('display', 'block');
+        $("#final-div").text("Game over! You got " + game.correctAnswerCounter + " out of " + questions.length + " questions correct.")
     }
 }
-
-//  THIS FUNCTION IS DONE FOR US!
-//  We do not need to touch it.
 
 function timeConverter(t) {
 
@@ -266,3 +284,8 @@ function timeConverter(t) {
 
     return minutes + ":" + seconds;
 }
+
+//Resets game when replay button is clicked
+$("#replay-button").on("click", function () {
+    location.reload();
+});
